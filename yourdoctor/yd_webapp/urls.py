be5ac3eedcb69a84b335376d94bc051241da0751
Liang He <2538931h@student.gla.ac.uk>
2021-08-05 02:44:36 +0100

    db_v4: add booking and doctor func into views.py

--- yourdoctor/yd_webapp/urls.py
+++ yourdoctor/yd_webapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('timetable/',views.timetable,name='timetable'),
-    path('user/',views.user,name='user'),
-    path('doctor/',views.doctor,name='doctor')
+    path('user/',views.useraccount,name='user'),
+    path('doctor/',views.doctor,name='doctor'),
+    path('booking/', views.booking, name='booking'),
 ]
--- yourdoctor/yourdoctor/urls.py
+++ yourdoctor/yourdoctor/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from  yd_webapp import views
+from yd_webapp import views
 
 
 urlpatterns = [
